8ca02f190d6ed11d4628745c35703da5ee19d712
Bret Marzolf <marzolfb@gmail.com>
2016-12-21 10:33:38 -0800

    Added jest snapshot tests for Pie chart

---
+++ src/__mocks__/react-native-svg.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+export default function({ children }) {
+  return (
+    <view mockedComponent="svg-component">
+      {children}
+    </view>
+  )
+}
+
+export const Rect = (props) => {
+  return (
+    <view mockedComponent="svg-Rect" {...props} />
+  )
+}
+
+export const Path = (props) => {
+  return (
+    <view mockedComponent="svg-Path" {...props} />
+  )
+}
+
+export const G = ({ children }) => {
+  return (
+    <view mockedComponent="svg-G">
+      {children}
+    </view>
+  )
+}
+
+export const Svg = (props) => {
+  return (
+    <view mockedComponent="svg-Svg" {...props} />
+  )
+}
+
+export const Text = (props) => {
+  return (
+    <view mockedComponent="svg-Text" {...props} />
+  )
+}
