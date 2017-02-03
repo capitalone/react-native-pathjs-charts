cf1562923141927f6bdc7095a0d3f919cd5370f6
Matt Labrum <mattl@mybudget.com.au>
2017-02-03 14:39:10 +1030

    Allow bar chart to show different color per bar

--- src/Bar.js
+++ src/Bar.js
@@ -117,7 +117,7 @@ export default class BarChart extends Component {
     let textStyle = fontAdapt(options.axisX.label)
 
     let lines = chart.curves.map(function (c, i) {
-      let color = this.color(i % 3)
+      let color = _.isArray(this.props.options.color) ? this.props.options.color[i] : this.color(i % 3)
       let stroke = Colors.darkenColor(color)
       return (
                 <G key={'lines' + i}>
