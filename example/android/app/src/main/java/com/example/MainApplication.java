211d645472c86b82c433cde559ff51ff9a38db57
Bret Marzolf <marzolfb@yahoo.com>
2017-11-12 17:56:51 -0800

    Fix android & jest

--- example/android/app/src/main/java/com/example/MainApplication.java
+++ example/android/app/src/main/java/com/example/MainApplication.java
@@ -27,11 +27,6 @@ public class MainApplication extends Application implements ReactApplication {
             new SvgPackage()
       );
     }
-
-    @Override
-    protected String getJSMainModuleName() {
-      return "index";
-    }
   };
 
   @Override
